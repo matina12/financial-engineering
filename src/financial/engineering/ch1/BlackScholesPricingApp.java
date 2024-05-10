dedf9ef0635efba2b9e142d65c392447af4a4ef3
Matina <matina.kosta1@gmail.com>
2024-05-11 00:31:11 +0300

    "commit"

--- src/financial/engineering/ch1/BlackScholesPricingApp.java
+++ src/financial/engineering/ch1/BlackScholesPricingApp.java
@@ -8,11 +8,17 @@ public class BlackScholesPricingApp {
 
     public static void main(String[] args) {
 
+        //Non dividends Black-Scholes
         double firstCallPrice=callPrice(28, 40, 0.06, Math.sqrt(0.5), 0.5);
         System.out.println("The price of this call option is: " + firstCallPrice);
 
         double firstPutPrice = putPrice(20, 20, 0.08, Math.sqrt(0.36), 0.5);
         System.out.println("The price of this call option is: " + firstPutPrice);
+
+        //Dividend Black-Scholes
+
+        double firstCallDiv = callPriceDiv(930, 900, 0.03, 0.08, 0.20, (double) 1 / 6);
+        System.out.println("The price of this call option is : " + firstCallDiv);
 
     }
 
@@ -32,7 +38,7 @@ public class BlackScholesPricingApp {
 
 
 
-
+    // Non dividends Black-Scholes Model
     public static double callPrice(double s, double x, double r, double sigma, double t){
         double callPriceBlack = 0;
         double d1 = (Math.log(s/x) + (r + (sigma * sigma)/2)*t)/ sigma * Math.sqrt(t);
@@ -48,4 +54,42 @@ public class BlackScholesPricingApp {
         putPriceBlack = x*Math.exp(-r*t)* cumulativeNormal(-d2) - s * cumulativeNormal(-d1) ;
         return putPriceBlack;
     }
+
+    //Dividends Black-Scholes Model
+    public static double callPriceDiv(double s, double x, double q ,double r, double sigma, double t){
+        double callPriceBlackDiv = 0;
+        double d1 = (Math.log(s/x) + (r - q + (sigma * sigma)/2)*t)/ sigma * Math.sqrt(t);
+        double d2 = d1 - sigma* Math.sqrt(t);
+        callPriceBlackDiv = s * Math.exp(-q*t) *cumulativeNormal(d1) - x*Math.exp(-r*t)* cumulativeNormal(d2);
+        return callPriceBlackDiv;
+    }
+
+    public static double putPriceDiv(double s, double x, double q ,double r, double sigma, double t){
+        double putPriceBlackDiv = 0;
+        double d1 = (Math.log(s/x) + (r - q + (sigma * sigma)/2)*t)/ sigma * Math.sqrt(t);
+        double d2 = d1 - sigma* Math.sqrt(t);
+        putPriceBlackDiv = x*Math.exp(-r*t)* cumulativeNormal(-d2) - s * Math.exp(-q*t) *cumulativeNormal(-d1) ;
+        return putPriceBlackDiv;
+    }
+
+    // European Currency Options
+
+    public static double callPriceCurr(double s, double x, double rf ,double r, double sigma, double t){
+        double callPriceBlackDiv = 0;
+        double d1 = (Math.log(s/x) + (r - rf + (sigma * sigma)/2)*t)/ sigma * Math.sqrt(t);
+        double d2 = d1 - sigma* Math.sqrt(t);
+        callPriceBlackDiv = s * Math.exp(-rf*t) *cumulativeNormal(d1) - x*Math.exp(-r*t)* cumulativeNormal(d2);
+        return callPriceBlackDiv;
+    }
+
+    public static double putPriceCurr(double s, double x, double rf ,double r, double sigma, double t){
+        double putPriceBlackDiv = 0;
+        double d1 = (Math.log(s/x) + (r - rf + (sigma * sigma)/2)*t)/ sigma * Math.sqrt(t);
+        double d2 = d1 - sigma* Math.sqrt(t);
+        putPriceBlackDiv = x*Math.exp(-r*t)* cumulativeNormal(-d2) - s * Math.exp(-rf*t) *cumulativeNormal(-d1) ;
+        return putPriceBlackDiv;
+    }
+
+
+
 }
